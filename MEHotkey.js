d0046a5c3133d7466bb7d8f5933d4d69b88b2c42
KaiserTorian <chrstianmeikl@gmail.com>
2026-01-06 00:41:57 +0100

    init

---
+++ MEHotkey.js
@@ -0,0 +1,18 @@
+for (var i = 0; i < 46; i++) {
+    var inv = Player.openInventory()
+    var item = Player.openInventory().getSlot(i)
+
+    if (item.getName().getString() == "Wireless Terminal") {
+
+        if (i != 44) { inv.swapHotbar(i, 8) }
+
+        var last_slot = inv.getSelectedHotbarSlotIndex()
+        inv.setSelectedHotbarSlotIndex(8)
+        Time.sleep(50)
+        Player.getInteractionManager().interact()
+        Time.sleep(50)
+        inv.setSelectedHotbarSlotIndex(last_slot) 
+    }
+    
+}
+
